642db955e843f5362c35c0a3ebfb85bdc3796674
Aishwarya Dharmadhikari <44116898+AishwaryaDharma@users.noreply.github.com>
2022-03-10 11:56:33 +0530

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,14 @@
+#oops1
+#CREATING CLASSES AND OBJECTS 
+        
+#1. CREATING CLASS with attributes(varibles)
+class person:
+    #attribute or variable
+    name="renu"  
+    age=20
+
+#actions means functions
+def talk(cls):
+    print(cls.name)
+    print(cls.age)
+    
